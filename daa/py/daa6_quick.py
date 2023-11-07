520b837a15e512e5ba10b8b9def95985eef91bb1
Hardikmaind <maindhardik@gmail.com>
2023-11-07 08:08:39 +0530

    latest

--- daa/py/daa6_quick.py
+++ daa/py/daa6_quick.py
@@ -1,44 +1,79 @@
-# Python3 implementation of QuickSort
-# Function to find the partition position
-def partition(array, low, high):
-	# Choose the rightmost element as pivot
-	pivot = array[high]
-	# Pointer for greater element
-	i = low - 1
-	# Traverse through all elements
-	# compare each element with pivot
-	for j in range(low, high):
-		if array[j] <= pivot:
-			# If element smaller than pivot is found
-			# swap it with the greater element pointed by i
-			i = i + 1
-			# Swapping element at i with element at j
-			(array[i], array[j]) = (array[j], array[i])
-	# Swap the pivot element with
-	# the greater element specified by i
-	(array[i + 1], array[high]) = (array[high], array[i + 1])
-	# Return the position from where partition is done
-	return i + 1
 
-# Function to perform quicksort
-def quicksort(array, low, high):
-	if low < high:
-		# Find pivot element such that
-		# element smaller than pivot are on the left
-		# element greater than pivot are on the right
-		pi = partition(array, low, high)
-		# Recursive call on the left of pivot
-		quicksort(array, low, pi - 1)
+def first_element_pivot_quicksort(arr):
+    if len(arr) <= 1:
+        return arr
 
-		# Recursive call on the right of pivot
-		quicksort(array, pi + 1, high)
+    pivot = arr[0]
+    less = [x for x in arr[1:] if x <= pivot]
+    greater = [x for x in arr[1:] if x > pivot]
 
-if __name__ == '__main__':
-	array = [10, 7, 8, 9, 1, 5]
-	N = len(array)
+    return first_element_pivot_quicksort(less) + [pivot] + first_element_pivot_quicksort(greater)
 
-	# Function call
-	quicksort(array, 0, N - 1)
-	print('Sorted array:')
-	for x in array:
-		print(x, end=" ")
+def last_element_pivot_quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    pivot = arr[-1]
+    less = [x for x in arr[:-1] if x <= pivot]
+    greater = [x for x in arr[:-1] if x > pivot]
+
+    return last_element_pivot_quicksort(less) + [pivot] + last_element_pivot_quicksort(greater)
+
+def median_of_three_pivot_quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    # Choose the pivot as the median of the first, middle, and last elements
+    first = arr[0]
+    middle = arr[len(arr) // 2]
+    last = arr[-1]
+    pivot = sorted([first, middle, last])[1]
+
+    less = [x for x in arr if x < pivot]
+    equal = [x for x in arr if x == pivot]
+    greater = [x for x in arr if x > pivot]
+
+    return median_of_three_pivot_quicksort(less) + equal + median_of_three_pivot_quicksort(greater)
+
+# Example usage:
+arr = [3, 6, 8, 10, 1, 2, 1]
+
+sorted_first_pivot = first_element_pivot_quicksort(arr)
+sorted_last_pivot = last_element_pivot_quicksort(arr)
+sorted_median_of_three_pivot = median_of_three_pivot_quicksort(arr)
+
+print("First Element Pivot Quicksort:", sorted_first_pivot)
+print("Last Element Pivot Quicksort:", sorted_last_pivot)
+print("Median-of-Three Pivot Quicksort:", sorted_median_of_three_pivot)
+
+
+import random
+
+def randomized_partition(arr, low, high):
+    pivot_index = random.randint(low, high)
+    arr[pivot_index], arr[high] = arr[high], arr[pivot_index]
+    pivot = arr[high]
+    i = low - 1
+
+    for j in range(low, high):
+        if arr[j] < pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
+
+def randomized_quicksort(arr, low, high):
+    if low < high:
+        pivot_index = randomized_partition(arr, low, high)
+        randomized_quicksort(arr, low, pivot_index - 1)
+        randomized_quicksort(arr, pivot_index + 1, high)
+
+# Example usage:
+arr = [3, 6, 8, 10, 1, 2, 1]
+randomized_quicksort(arr, 0, len(arr) - 1)
+print("Randomized element Sorted array:", arr)
+
+
+
+
